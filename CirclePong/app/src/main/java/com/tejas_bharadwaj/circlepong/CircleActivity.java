b2ee7931743d8602c732fda1ceb50d96d314445f
tejaskb04 <tejaskb@gmail.com>
2017-09-15 17:28:41 -0700

    Button Code

--- CirclePong/app/src/main/java/com/tejas_bharadwaj/circlepong/CircleActivity.java
+++ CirclePong/app/src/main/java/com/tejas_bharadwaj/circlepong/CircleActivity.java
@@ -2,12 +2,20 @@ package com.tejas_bharadwaj.circlepong;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class CircleActivity extends AppCompatActivity {
+
+    private GameView gameView;
+    private Button leftBtn;
+    private Button rightBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circle);
+        gameView = (GameView) findViewById(R.id.game_view);
+        leftBtn = (Button) findViewById(R.id.left);
+        rightBtn = (Button) findViewById(R.id.right);
     }
 }
